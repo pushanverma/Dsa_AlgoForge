8bcac3e05b913e2d2ecce3c9a31519f065c251d7
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-02-09 19:48:56 +0530

    first commit

---
+++ Lec_1_Feb4/Average.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Average {
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+
+        // taking integer inputs from user 
+
+        System.out.println("Enter three Numbers -------->");
+        int a =scn.nextInt();
+        int b =scn.nextInt();
+        int c=scn.nextInt();
+
+        // Average can be decimal also , therefore making it double
+
+        double average= (a+b+c)/3;
+
+        System.out.println("Average-"+average);
+
+
+
+    
+    }
+}
+
