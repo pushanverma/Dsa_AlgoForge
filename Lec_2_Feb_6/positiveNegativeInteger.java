06e1e9114c8529f3186fe2e74adccea24e302297
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-02-28 22:17:15 +0530

    lec_2 positiveNegative Integer added

---
+++ Lec_2_Feb_6/positiveNegativeInteger.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class positiveNegativeInteger {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter number =");
+        int n = scn.nextInt();
+
+        if (n < 0) {
+            System.out.println("Negative  number ");
+        } else {
+            System.out.println("Positive number ");
+        }
+    }
+}
