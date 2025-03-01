77fb0e5de6a24eab580a464edf3d8e2af6e31bf3
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-03-02 00:27:03 +0530

    loop examples added

---
+++ Lec_3_Feb_8/LoopExample.java
@@ -0,0 +1,25 @@
+
+public class LoopExample {
+
+    public static void main(String[] args) {
+
+        //printing 1 to 5 using while Loop 
+
+        System.out.println("---------------using while loop-------------------");
+
+
+        int a=1;
+        while(a<=5){
+            System.out.println(a);
+            a++;
+        }
+
+
+        System.out.println("---------------using for loop-------------------");
+        // printing 1 to 5 using for loop 
+
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
