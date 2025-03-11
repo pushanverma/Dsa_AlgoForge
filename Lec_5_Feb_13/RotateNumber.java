d8437e9f03ed05223bb29989fa555fe245014764
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-03-11 17:01:32 +0530

    rotate number code added

--- Lec_5_Feb_13/RotateNumber.java
+++ Lec_5_Feb_13/RotateNumber.java
@@ -1,27 +1,46 @@
 import java.util.*;
 
-public class RotateNumber{
+public class RotateNumber {
 
     public static void main(String[] args) {
-        
 
         Scanner scn = new Scanner(System.in);
 
+        System.out.print("Enter number= ");
+        int num = scn.nextInt();
 
-       // input of number 
-        int n =scn.nextInt();
+        int originalNum = num;
 
+        System.out.println("Enter rotation number =");
+        int k = scn.nextInt();
 
-        //rotation Number 
-        int k =scn.nextInt();
+        // count of digits
+        int count = 0;
+        while (num > 0) {
+            num = num / 10;
+            count++;
+        }
 
+        k = k % count; // making number smaller , irrespective of whether its negative or positive
 
-         // Number can be very big , converting it to short
+        if (k < 0) {
+            k = k + count; // if number is negative
+        }
 
-         
+        int ans = originalNum;
 
+        int rotationDone = 0;
 
+        while (rotationDone < k) {
+
+            int lastDigit = ans % 10;
+            ans = ans / 10;
+            ans = ans + lastDigit * (int) Math.pow(10, count - 1);
+
+            rotationDone++;
+        }
+
+        System.out.println(ans);
 
     }
-
 }
