3f060d2f66cd1e1cff8bb2aee50c9cdf79e85db9
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-04-07 12:17:56 +0530

    printDecreasing code added

---
+++ Lec_17_March_18/PrintDecreasing.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PrintDecreasing {
+
+    public static void printDecreasing(int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+        printDecreasing(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter number=");
+        int n = scn.nextInt();
+
+        printDecreasing(n);
+
+    }
+
+}
