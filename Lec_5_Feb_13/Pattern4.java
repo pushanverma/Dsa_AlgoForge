4f1728ba682d646f58307f02e9e79baabba1059b
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-05-03 17:49:43 +0530

    updated pattern4

--- Lec_5_Feb_13/Pattern4.java
+++ Lec_5_Feb_13/Pattern4.java
@@ -1,36 +1,34 @@
 import java.util.*;
 
 public class Pattern4 {
-    
+
     public static void main(String[] args) {
-        
-        Scanner scn =new Scanner(System.in);
+
+        Scanner scn = new Scanner(System.in);
 
         System.out.println("Enter lines --->");
-        int n =scn.nextInt();
+        int n = scn.nextInt();
 
-        int total_number_of_line=n;
+        int total_number_of_line = n;
 
+        int current_line = 1;
+        int stars = n;
+        int spaces = 0;
 
-        int current_line=1;
-        int stars=n;
-        int spaces=0;
+        while (current_line <= total_number_of_line) {
 
-        while(current_line<=total_number_of_line){
-
-            // print spaces 
-            for(int i=1;i<=spaces;i++){
+            // print spaces
+            for (int i = 1; i <= spaces; i++) {
                 System.out.print("\t");
             }
 
-
             // print stars
 
-            for(int i=1;i<=stars;i++){
+            for (int i = 1; i <= stars; i++) {
                 System.out.print("*\t");
             }
 
-            // move to next Line , increase number of stars and decrease number of spaces 
+            // Prepare the next Line
             System.out.println();
             stars--;
             spaces++;
