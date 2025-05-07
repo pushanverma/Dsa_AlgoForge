0192993f6e681acc74ec0856095bc2c4311a48cd
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-05-07 20:20:38 +0530

    Pattern 15 code added

---
+++ Lec_7_Feb_16/Pattern15.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Pattern15 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int tnol = n;
+        int cnol = 1;
+        int mid = (n + 1) / 2;
+
+        int stars = n;
+        int space = 0;
+
+        while (cnol <= tnol) {
+
+            // print spaces
+            for (int i = 1; i <= space; i++) {
+                System.out.print("\t");
+            }
+
+            // print stars
+            for (int i = 1; i <= stars; i++) {
+
+                // half
+                if (cnol > 1 && cnol <= n / 2) {
+
+                    if (i == 1 || i == stars) {
+                        System.out.print("*\t");
+                    } else {
+                        System.out.print("\t");
+                    }
+
+                } else {
+                    System.out.print("*\t");
+                }
+
+            }
+
+            // prepare for the next line
+
+            if (cnol < mid) {
+                space++;
+                stars -= 2;
+            } else {
+                space--;
+                stars += 2;
+            }
+
+            cnol++;
+            System.out.println();
+
+        }
+
+    }
+
+}
