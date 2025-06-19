1a9b2cf9ef0d33ccde07e6a38b3ca5ebf692bdc2
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-06-19 20:56:44 +0530

    firstIndex code added

--- 5.Recursion and Backtracking/firstIndex.java
+++ 5.Recursion and Backtracking/firstIndex.java
@@ -2,22 +2,38 @@ import java.util.*;
 
 public class firstIndex {
 
+    // topic: "On the way Down" Approach
+
+    // public static int findfirstIndex(int[] arr, int idx, int target) {
+
+    // // base case
+    // if (idx == arr.length) { //1
+    // return -1;
+    // }
+
+    // int firstIndexsoFar = findfirstIndex(arr, idx + 1, target); //2
+
+    // if (arr[idx] == target) { //3
+    // return idx;
+    // } else {
+    // return firstIndexsoFar;
+    // }
+    // }
+
+    // topic: "On the way Up" Approach(More optimized)- it will find the index
     public static int findfirstIndex(int[] arr, int idx, int target) {
 
-
-        if(idx==arr.length ){
+        if (idx == arr.length) { // 1
             return -1;
         }
 
-        if(arr[idx]==target){
+        if (arr[idx] == target) { //2
             return idx;
         }
 
-       
+        int firstIndexsoFar = findfirstIndex(arr, idx + 1, target); //3
 
-        findfirstIndex(arr,idx+1,target);
-
-        
+        return firstIndexsoFar;
 
     }
 
@@ -25,9 +41,14 @@ public class firstIndex {
 
         Scanner scn = new Scanner(System.in);
 
-        int[] arr = { 9, 10, 2, 3, 2, 2, 4, 5 };
+        int[] arr = { 9, 10, 3, 2, 2, 4, 5 };
 
-        System.out.print("Enter target =");
+        // printing array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+
+        System.out.print("\nEnter target =");
         int target = scn.nextInt();
 
         int ans = findfirstIndex(arr, 0, target);
