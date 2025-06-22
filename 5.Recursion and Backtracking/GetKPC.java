7245c86b15250977f4f0b11a26abf23bcef146e1
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-06-23 00:51:03 +0530

    added info

--- 5.Recursion and Backtracking/GetKPC.java
+++ 5.Recursion and Backtracking/GetKPC.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class GetKPC {
 
+    //Leetcode 17 -https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
     static String[] mobile_letters_array = { ",:", "<;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
     public static ArrayList<String> getAllKPC(String str) {
 
-        // base case
+        // base case                                    //1
         if (str.length() == 0) {
             ArrayList<String> baseAns = new ArrayList<>();
             baseAns.add("");
@@ -14,18 +16,18 @@ public class GetKPC {
 
         }
 
-        int firstDigit = str.charAt(0) - '0'; // digit - 4
+        int firstDigit = str.charAt(0) - '0'; // digit - 4 (Here the ASCII values are subtracted) //2
 
-        String remainingString = str.substring(1); // String - "56"
+        String remainingString = str.substring(1); // String - "56"   //3
 
-        ArrayList<String> smallAns = getAllKPC(remainingString); // recursion call 
+        ArrayList<String> smallAns = getAllKPC(remainingString); // recursion call   //4
 
-        ArrayList<String> final_subsequence = new ArrayList<>(); // final arraylist to be returned 
+        ArrayList<String> final_subsequence = new ArrayList<>(); // final arraylist to be returned   //5
 
-        String current_letter = mobile_letters_array[firstDigit]; // String - "ghi" 
+        String current_letter = mobile_letters_array[firstDigit]; // String - "ghi"   //6 
 
 
-        for (int i = 0; i < current_letter.length(); i++) {
+        for (int i = 0; i < current_letter.length(); i++) {  //7
 
             char firstLetter = current_letter.charAt(i);
 
