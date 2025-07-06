7302d25b19eec585fbca677d6c4c2db57d206353
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-07-06 12:50:32 +0530

    getMazePathwithJumps code added

---
+++ 5.Recursion and Backtracking/GetMazePathWithJumps.java
@@ -0,0 +1,61 @@
+
+import java.util.*;
+
+public class GetMazePathWithJumps {
+
+    public static ArrayList<String> getMazePathwithJumps(int sr, int sc, int dr, int dc) {
+
+        //base Case
+        if (sr > dr || sc > dc) {
+            return new ArrayList<>();
+        }
+
+        if (sr == dr && sc == dc) {
+            ArrayList<String> baseAns = new ArrayList<>();
+            baseAns.add("");
+            return baseAns;
+        }
+
+        ArrayList<String> allPaths = new ArrayList<>();
+
+        //horizontal path 
+        for (int jump = 1; jump <= dc - sc; jump++) {
+            ArrayList<String> hpath = getMazePathwithJumps(sr, sc + jump, dr, dc);
+
+            for (String path : hpath) {
+                allPaths.add("h" + jump + path);
+            }
+        }
+
+        //vertical paths
+        for (int jump = 1; jump <= dr - sr; jump++) {
+            ArrayList<String> vpath = getMazePathwithJumps(sr + jump, sc, dr, dc);
+
+            for (String path : vpath) {
+                allPaths.add("v" + jump + path);
+            }
+        }
+
+        //diagonal paths 
+        for (int jump = 1; jump <= Math.min(dr, dc); jump++) {
+            ArrayList<String> dpath = getMazePathwithJumps(sr + jump, sc + jump, dr, dc);
+
+            for (String path : dpath) {
+                allPaths.add("d" + jump + path);
+            }
+        }
+
+        return allPaths;
+        
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> ans = getMazePathwithJumps(0, 0, 2, 2);
+        System.out.println(ans);
+        System.out.println("size->"+ ans.size());
+
+    }
+
+}
