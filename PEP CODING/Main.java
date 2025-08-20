784f04bd0bbb58b8eacf5c26dc0dea6735177ba5
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2025-08-20 14:50:28 +0530

    adding loop in Main test program

--- PEP CODING/Main.java
+++ PEP CODING/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world !! ");
+
+        System.out.println("Printing counting from 1 to 10 ");
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
        
     }
 }
