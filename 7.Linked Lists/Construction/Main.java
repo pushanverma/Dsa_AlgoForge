ee52a44a13fb4d9ee71623e1d0ac928e76c69939
PUSHAN VERMA <53814344+pushanverma@users.noreply.github.com>
2026-06-06 17:15:55 -0400

    understanding LinkedLists

--- 7.Linked Lists/Construction/Main.java
+++ 7.Linked Lists/Construction/Main.java
@@ -1,89 +1,66 @@
-
-
-//note:---------------  NODE CLASS ------------
- class Node {
-
+// A Single Node contains two data - one is Data and other one is the address of Next Node 
+class Node {
     int data;
     Node next;
 
-    // empty Constructor
-    public Node() {
-
-    }
-
-    // constructor(Node)
+    // parametrized constructor
     public Node(int data) {
         this.data = data;
-        this.next = null;
     }
-}
-
-//note:------------ LINKED LISTS CLASS ------------
-
-class LinkedList {
-    //variables 
-
-    Node head;
-    Node tail;
-    int size;
-
-    // constructor( Linked_list)
-    public LinkedList() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    // function - addLast
-
-    public void addLast(int data) {
-        //Firstly creating new Node which contains data 
-        
-        Node newNode = new Node(data);
-
-        // note : always in LL , look at three cases
-
-        if (head == null) {
-            // 1. when there are no Nodes(Empty)
-            head = newNode;
-            tail=newNode;
-
-        } else if (head == tail) {
-            // 2. Only one Node is present
-            head.next=newNode;
-            tail=newNode;
-        } else {
-            // 3. Rest all cases
-             head.next=newNode;
-             tail=newNode;
-        }
-
-
-    }
-
-    public String toString(){
-        String list = "";
-
-        Node temp = head;
-        while(temp != null){
-            list += temp.data + ", ";
-            temp = temp.next;
-        }
-
-        return list;
-    }
-
 }
 
 public class Main {
 
-    LinkedList ll = new LinkedList();
+    public static void main(String [] args){
 
-    ll.addLast(10);
-    ll.addLast(20);
-    ll.addLast(30);
+        // 3 Nodes are made at a random place inside the Heap 
+        Node n1 = new Node(10);   // 4k 
+        Node n2 = new Node(20);   // 5k 
+        Node n3 = new Node(30);   // 6k 
 
-    System.out.println(ll);
-    
+        //note: The First guy is also called the "Head" of the Linked List (In most of question , we will only be getting the Head )
+        
+        n1.next =n2;      // n1.next stores 5k 
+        n2.next =n3;      // n2.next stores 6k 
+
+
+        Node firstGuy = n1;     
+        System.out.println(n1 + "...first Guy ");
+        System.out.println(n1.data +"...data of First Guy ");
+        System.out.println(n1.next + "...Address of Second guy");
+
+        System.out.println("___________");
+        
+        Node secondGuy= n1.next; // next of n1 will store the address of n2 
+        System.out.println(secondGuy+"...Second Guy");
+        System.out.println(secondGuy.data+" ...data of Second guy");
+        System.out.println(secondGuy.next+"...Address of Third Guy");
+        
+        System.out.println("___________");
+
+        Node thirdGuy = secondGuy.next;
+        System.out.println(thirdGuy+"...third Guy ");
+        System.out.println(thirdGuy.data+"...data of third Guy");
+        System.out.println(thirdGuy.next+"....Address of Third Guy");
+
+
+
+
+
+
+
+
+
+      
+
+        
+
+
+
+
+
+
+
+    }
 
 }
